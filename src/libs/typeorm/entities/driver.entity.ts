aae5a99cf3bc608c00f8dbce6294124fb8cb2e9c
lueberGandra <lueber_emp@hotmail.com>
2024-03-07 06:56:32 -0300

    fix: changes drivers entity name

--- src/libs/typeorm/entities/driver.entity.ts
+++ src/libs/typeorm/entities/driver.entity.ts
@@ -1,6 +1,6 @@
 import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
-@Entity('driver')
+@Entity('drivers')
 export class Driver {
     @PrimaryGeneratedColumn()
     id: number;
